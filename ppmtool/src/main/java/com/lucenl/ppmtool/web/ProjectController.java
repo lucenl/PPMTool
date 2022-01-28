30fa7743522730655ed9f8f44da903d5e21684a1
Li Lucen <lilucen@LideMacBook-Pro.local>
2022-01-28 01:02:05 -0800

    MapValidationErrorService

--- ppmtool/src/main/java/com/lucenl/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/lucenl/ppmtool/web/ProjectController.java
@@ -1,18 +1,22 @@
 package com.lucenl.ppmtool.web;
 
 import com.lucenl.ppmtool.domain.Project;
+import com.lucenl.ppmtool.services.MapValidationErrorService;
 import com.lucenl.ppmtool.services.ProjectService;
-import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -22,14 +26,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     //Allows us to actually have more control on our Jason responses.
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
-        if(result.hasErrors()){
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
-        }
 
-
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return errorMap;
+        
         Project project1 = projectService.saveOrUpdateProject((project));
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
