93d80be8929941772aa273b6f154421fab9caeba
Li Lucen <lilucen@LideMacBook-Pro.local>
2022-01-28 02:34:17 -0800

    Delete Project by Identifier

--- ppmtool/src/main/java/com/lucenl/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/lucenl/ppmtool/services/ProjectService.java
@@ -37,4 +37,14 @@ public class ProjectService {
     public Iterable<Project> findAllProjects(){
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectid){
+        Project project = projectRepository.findByProjectIdentifier(projectid.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Cannot Project with ID '" + projectid + "'. This project does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
 }
