c30ce599dd6e2a95fa673a33796d63febfb06e78
Li Lucen <lilucen@LideMacBook-Pro.local>
2022-01-28 01:51:26 -0800

    Custom Exceptions for Unique Project Identifier

--- ppmtool/src/main/java/com/lucenl/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/lucenl/ppmtool/services/ProjectService.java
@@ -1,9 +1,11 @@
 package com.lucenl.ppmtool.services;
 
 import com.lucenl.ppmtool.domain.Project;
+import com.lucenl.ppmtool.exceptions.ProjectIdException;
 import com.lucenl.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ProjectService {
@@ -12,9 +14,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-
-        //Logic
-
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID'" + project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+        
     }
 }
--- ppmtool/src/main/java/com/lucenl/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/lucenl/ppmtool/web/ProjectController.java
@@ -14,9 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 @RestController
