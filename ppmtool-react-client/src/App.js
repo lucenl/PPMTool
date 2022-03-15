592c86cdf983f7ad2029937a7ef5c5de6c8b161e
Li Lucen <lilucen@LideMacBook-Pro.local>
2022-03-15 11:31:44 -0700

    Create React and dashboard components

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+      <Dashboard />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
