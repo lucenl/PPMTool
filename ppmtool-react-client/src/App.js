5bab0af40e2becbfb5218be812f4bd2ffc04c6d4
Li Lucen <lilucen@LideMacBook-Pro.local>
2022-06-29 23:54:07 -0700

    Set HTML design for Dashboard.js and ProjectItem.js; Set icons of project items and app title in index.html

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
