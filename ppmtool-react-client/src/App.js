997d59183d2953d2bbb020341bef53a542b5b8ce
Li Lucen <lilucen@LideMacBook-Pro.local>
2022-06-29 18:47:04 -0700

    Create ProjectItem and put into Dashboard.js; Create Navbar and put into App.js

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
-      <Dashboard />
+      <Header />
       <Dashboard />
     </div>
   );
