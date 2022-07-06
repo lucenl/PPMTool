b72beab1fad221ca2493ca6de2694da5943167c0
Li Lucen <lilucen@LideMacBook-Pro.local>
2022-07-06 16:25:56 -0700

    Change AddProject to controlled component; set/change state in input field; set OnSubmit for the form

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -1,10 +1,113 @@
 import React, { Component } from 'react'
 
 class AddProject extends Component {
+  constructor(){
+    super()
+
+    // set up an initial state for all of our input fields
+    this.state = {
+      projectName: "",
+      projectIdentifier: "",
+      description: "",
+      start_date: "",
+      end_date: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+  }
+  // State is immutable. The only way to change a state is to set a new state.
+  onChange(e){
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e){
+    // Prevent the form from reloading
+    e.preventDefault();
+
+    const newProject = {
+      projectName: this.state.projectName,
+      projectIdentifier: this.state.projectIdentifier,
+      description: this.state.description,
+      start_date: this.state.start_date,
+      end_date: this.state.end_date
+    }
+    
+    // Grab the new Project object that we created here and react and manually send it to our back end through Postman
+    console.log(newProject);
+  }
+
   render() {
     return (
       <div>
-        <h1>Add Projet Form</h1>
+        {
+
+        }
+        <div className="project">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8 m-auto">
+                    <h5 className="display-4 text-center">Create Project form</h5>
+                    <hr />
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <input 
+                            type="text" 
+                            className="form-control form-control-lg " 
+                            placeholder="Project Name" 
+                            name="projectName"
+                            value={this.state.projectName}
+                            onChange={this.onChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input 
+                            type="text" 
+                            className="form-control form-control-lg" 
+                            placeholder="Unique Project ID" 
+                            name="projectIdentifier"
+                            value={this.state.projectIdentifier}
+                            onChange={this.onChange}
+                            />
+                        </div>
+                        
+                        <div className="form-group">
+                            <textarea 
+                            className="form-control form-control-lg" 
+                            placeholder="Project Description" 
+                            name="description"
+                            value={this.state.description}
+                            onChange={this.onChange}
+                            ></textarea>
+                        </div>
+                        <h6>Start Date</h6>
+                        <div className="form-group">
+                            <input 
+                            type="date" 
+                            className="form-control form-control-lg" 
+                            name="start_date"  
+                            value={this.state.start_date}
+                            onChange={this.onChange}
+                            />
+                        </div>
+                        <h6>Estimated End Date</h6>
+                        <div className="form-group">
+                            <input 
+                            type="date" 
+                            className="form-control form-control-lg" 
+                            name="end_date" 
+                            value={this.state.end_date}
+                            onChange={this.onChange}
+                            />
+                        </div>
+
+                        <input type="submit" className="btn btn-primary btn-block mt-4" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
       </div>
     )
   }
