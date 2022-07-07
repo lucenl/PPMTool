2f1eae5f14d71953e83ce9f74adff0b43c652b7b
Li Lucen <lilucen@LideMacBook-Pro.local>
2022-07-06 19:02:33 -0700

    Create Project from React

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {createProject} from "../../actions/projectActions";
 
 class AddProject extends Component {
   constructor(){
@@ -35,7 +38,9 @@ class AddProject extends Component {
     }
     
     // Grab the new Project object that we created here and react and manually send it to our back end through Postman
-    console.log(newProject);
+    //console.log(newProject);
+
+    this.props.createProject(newProject, this.props.history)
   }
 
   render() {
@@ -113,4 +118,11 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+  createProject : PropTypes.func.isRequired
+}
+
+export default connect(
+  null, 
+  { createProject }
+)(AddProject);
