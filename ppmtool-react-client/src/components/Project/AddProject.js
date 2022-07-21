1a66d8356645e694aad7710e00e716bc1285a50e
Li Lucen <lilucen@LideMacBook-Pro.local>
2022-07-21 00:12:19 -0700

    style validation errors with classnames

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {createProject} from "../../actions/projectActions";
+import classnames from "classnames"
 
 class AddProject extends Component {
   constructor(){
@@ -68,13 +69,19 @@ class AddProject extends Component {
                         <div className="form-group">
                             <input 
                             type="text" 
-                            className="form-control form-control-lg " 
+                            className={classnames("form-control form-control-lg ", {
+                              "is-invalid": errors.projectName
+                            }) }
                             placeholder="Project Name" 
                             name="projectName"
                             value={this.state.projectName}
                             onChange={this.onChange}
                             />
-                            <p>{errors.projectName}</p>
+                            {errors.projectName && (
+                              <div className="invalid-feedback">
+                              {errors.projectName}
+                              </div>
+                            )}
                         </div>
                         <div className="form-group">
                             <input 
